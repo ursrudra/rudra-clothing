39a6bbb3873f8bc1c5be4a493bae239da9254f89
Rudra Kishore <giduturirudrakishore@gmail.com>
2019-07-28 10:52:22 +0530

    changed the email sign in handler from redux thunk to redux saga

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import './signin.styles.scss'
-import { auth,SigninWithGoogle } from '../../firebase/firebase-utils';
-import { googleSignInStart } from '../../redux/user/user.action';
+import { googleSignInStart, emailSignInStart } from '../../redux/user/user.action';
 import { connect } from 'react-redux';
 class Signin extends Component {
     constructor(props) {
@@ -18,15 +17,10 @@ class Signin extends Component {
     handleSubmit = async (event) => {
         event.preventDefault();
         const {email,password} = this.state;
+        const { emailSignInStart} = this.props;
+        emailSignInStart(email,password);
 
-        try {
-            await auth.signInWithEmailAndPassword(email,password);
-            this.setState({email:'',password:'',error:''});
-        } catch (error) {
-            console.log(error);
-            this.setState({error:error.message})
-            
-        }
+        
 
     }
     handleChange = event => {
@@ -72,6 +66,7 @@ class Signin extends Component {
 }
 
 const mapDispatchToProps = dispatch =>({
-    googleSignInStart: ()=> dispatch(googleSignInStart())
+    googleSignInStart: ()=> dispatch(googleSignInStart()),
+    emailSignInStart: (email,password) => dispatch(emailSignInStart({email,password}))
 })
 export default connect(null,mapDispatchToProps)(Signin)
